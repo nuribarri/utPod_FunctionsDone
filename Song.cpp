dc0dfc38495fcd7749c63ac5f548546815d65d42
nuribarri <njuribarri@utexas.edu>
2020-04-02 09:24:19 -0600

    Backbone

--- Song.cpp
+++ Song.cpp
@@ -5,6 +5,82 @@
 
 
 
-using namespace std; 
+using namespace std;
+
+
+Song::Song() {
+
+    artist = "";
+    title = "";
+    size = MAX_SIZE;
+
+}
+
+Song::Song(string artist) {
+
+    this->artist = artist;
+    title = "";
+    size = MAX_SIZE;
+
+}
+
+Song::Song(string _artist, string _title, int _size) {
+
+    artist = _artist;
+    title = _title;
+    size = _size;
+
+}
+
+string Song::getTitle() const {
+    return title;
+}
+
+void Song::setTitle(string name) {
+
+    title = name;
+}
+
+int Song::getSize() const {
+    return  size;
+}
+
+void Song::setSize(int s) {
+
+    this->size = s;
+}
+
+void Song::swap(Song &p) {
+
+    Song temp = p;
+    p = *this;
+    *this = temp;
+}
+
+bool Song::operator>(Song const &rhs) {
+    return (size > rhs.size);
+}
+
+bool Song::operator==(Song const &rhs) {
+
+    return (artist == rhs.artist &&
+            title == rhs.title &&
+            size == rhs.size);
+}
+
+Song::~Song() {
+
+    cout << "debug - in destructor for " << artist << endl;
+}
+
+    ostream& operator << (ostream& out, const Song &s)
+    {
+        out << s.getArtist() << " (" << s.getTitle() <<
+        ") - " << s.getSize();
+        return out;
+    }
+
+
+
 
 
--- Song.h
+++ Song.h
@@ -3,32 +3,57 @@
 #ifndef Song_H
 #define Song_H
 
+#include <string>
+#include <iostream>
+
+using namespace std;
+
 
 class Song
 {
 
+    private:
+     string artist;
+     string title;
+     int size;
+
+     static const int MAX_SIZE = 512;
+
+    public:
+     Song();
+     Song(string artist);
+     Song(string artist, string title, int size);
+
+     string getArtist() const
+     { return artist; }
 
 
+     void setArtist( string a )
+     {
+        artist = a;
+     }
 
 
+     string getTitle() const;
 
+     void setTitle( string name);
 
+     int getSize() const;
 
+     void setSize( int s);
 
+     void swap(Song &p);
 
+     bool operator >(Song const &rhs);
+     bool operator ==(Song const &rhs);
 
-
-
-
-
-
-
-
-
-
-
+     ~Song();
 
 };
+
+    ostream& operator << (ostream& out, const Song &g);
+
+
 
 
 #endif
